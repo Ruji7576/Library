9f31c0e235eba6b3cb227b3b15d5f8f32aaf6477
Ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-06-16 15:23:51 +0200

    Library

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    public static ArrayList<Book> bookArrayList = new ArrayList<>();
+    public static void main(String[] args) {
+    }
+}
